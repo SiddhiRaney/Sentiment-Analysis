4c085b76e61d7442c32f662655d1d8fd50490e6d
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-01-18 22:29:08 +0530

    Update Analyzer1.py

--- Analyzer1.py
+++ Analyzer1.py
@@ -1,18 +1,25 @@
-#nltk toolikit- VADER is used
+# nltk toolkit - VADER is used
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 sia = SentimentIntensityAnalyzer()
 
-#Comment is any piece of text/information for which we want to analyze the sentiment
+# Comment is any piece of text/information for which we want to analyze the sentiment
 def analyze_sentiment(comment):
     sentiment_scores = sia.polarity_scores(comment)
 
-    # Interpreting the sentiment
+    # The compound score is a single value that combines the positive, negative, and neutral scores
+    # into a normalized value ranging from -1 (most negative) to +1 (most positive).
+
+    # If the compound score is greater than or equal to 0.05, the overall sentiment is considered Positive.
     if sentiment_scores['compound'] >= 0.05:
         sentiment = 'Positive'
+
+    # If the compound score is less than or equal to -0.05, the overall sentiment is considered Negative.
     elif sentiment_scores['compound'] <= -0.05:
         sentiment = 'Negative'
+
+    # If the compound score falls between -0.05 and 0.05, the overall sentiment is considered Neutral.
     else:
         sentiment = 'Neutral'
 
@@ -23,9 +30,9 @@ def analyze_sentiment(comment):
 
 while True:
     comment = input("Enter a comment: ")
-    
+
     sentiment, sarcasm, compound_score = analyze_sentiment(comment)
-    
+
     # Output Results
     print(f"\nComment: {comment}")
     print(f"Sentiment: {sentiment}")
@@ -34,7 +41,7 @@ while True:
 
     # Ask if the user wants to continue or break
     continue_input = input("Do you want to continue (Y/N)? ").strip().lower()
-    
+
     if continue_input != 'y':
         print("Exiting the program. Goodbye!")
         break
